3d97805f516a704b49839d8837fa1cde493e7996
Jack Mitchell <jack@communistcode.co.uk>
2012-02-16 12:57:50 +0000

    initial commit

---
+++ torrentDecoder.class.php
@@ -0,0 +1,62 @@
+<?php
+
+  // Path to Bencode Class
+  $bencodePath = __DIR__."/../bencode/lib/Bencode.php";
+
+  // Include Bencode class
+  require_once($bencodePath);
+
+  class torrentDecoder {
+
+    //Torrent in decoded form
+    private $decodedTorrent;
+
+    function  __construct($torrentFile) {
+
+      // Check that what has been passed is a file
+      if (is_file($torrentFile)) {
+
+        $torrentFileContents = file_get_contents($torrentFile);
+
+        // Attempts to decode it
+        $this->decodedTorrent = Bencode::decode($torrentFileContents);
+      
+      } else {
+
+        throw new Exception("Argument passed was not a file!");
+
+      }
+
+    }
+
+    public function getFiles() {
+  
+      if (isset($this->decodedTorrent['info']['files'])) {
+
+        $torrentFiles = array();
+
+        foreach ($this->decodedTorrent['info']['files'] as $fileArray) {
+
+          foreach($fileArray['path'] as $path) {
+
+            array_push($torrentFiles,$path);
+
+          }  
+
+        }
+
+      }
+      
+      return $torrentFiles;
+
+    }
+
+    public function getDecodedTorrent() {
+
+      return $this->decodedTorrent;
+
+    }
+
+  }
+
+?>
